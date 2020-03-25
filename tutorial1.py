a7de18a8c353e24a3fbb53c4a88a3eb05c03149e
oliver <oliver.lell@gmx.at>
2020-03-25 14:28:41 +0100

    ss

--- tutorial1.py
+++ tutorial1.py
@@ -9,5 +9,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
+    gems= [3,5,7,8]
+    s.sendall(gems)
+
+
 
 print('Received', repr(data))
