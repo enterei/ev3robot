3b7e6dbf988e52c7e4c8dc2103effcad329cb3cf
oliver <oliver.lell@gmx.at>
2020-04-04 13:50:23 +0200

    ss

--- Robot.py
+++ Robot.py
@@ -3,6 +3,8 @@ from ColorSensor import ColorSensor
 import ev3dev.ev3 as ev3
 
 stop_action= "coast"
+power = 200
+dt = 5500
 
 class Robot:
     socket = MySocket()
@@ -11,12 +13,12 @@ class Robot:
     assert lm.connected  # left motor
     rm = ev3.LargeMotor('outC');
     assert rm.connected  # right motor
-    dt = 5500
+    #dt = 5500
 
 
     def run(self):
-        self.lm.run_timed(speed_sp = 100,time_sp=self.dt,stop_action=stop_action)
-        self.rm.run_timed(speed_sp = 100,time_sp=self.dt,stop_action=stop_action)
+        self.lm.run_timed(speed_sp = power,time_sp=dt,stop_action=stop_action)
+        self.rm.run_timed(speed_sp = power,time_sp=dt,stop_action=stop_action)
 
         print("run")
 
