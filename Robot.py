49d3cbdcf9ac3d29ea2acb615aec500fb170cf1c
oliver <oliver.lell@gmx.at>
2020-04-09 12:22:52 +0200

    linefollow trys

--- Robot.py
+++ Robot.py
@@ -120,7 +120,7 @@ class Robot:
         tank.cs=ev3.ColorSensor()
         print(dict(kwargs))
         tank.follow_line(
-        kp=0.3, ki=0.05, kd=3.2,
+        kp=0.3, ki=0.05, kd=1.2,
         speed=SpeedPercent(kwargs.get('speed')),
         follow_for=follow_for_ms,
         ms=ga
