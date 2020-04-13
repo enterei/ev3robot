5745105c2faf8a1122a1c7b8859a39dfeac9075c
oliver <oliver.lell@gmx.at>
2020-04-13 13:44:34 +0200

    linefollow trys

--- Robot.py
+++ Robot.py
@@ -120,6 +120,7 @@ class Robot:
         tank = myMotor.MoveTank(OUTPUT_A,OUTPUT_B)
         tank.cs=ev3.ColorSensor()
         print(dict(kwargs))
+        print(kwargs.get('turn'))
 
         res= tank.follow_line(
          #   kp=0.3, ki=0.05, kd=0.2,
