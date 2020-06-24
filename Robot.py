c3f4ab93fe6a6724efcfa263e4fa9a46f84d2b54
oliver <oliver.lell@gmx.at>
2020-06-24 19:02:22 +0200

    testturn

--- Robot.py
+++ Robot.py
@@ -59,7 +59,7 @@ class Robot:
         print("robo cmode: " + str(self.cs.mode))
     def makesound(self):
         #Sound.tone([(264, 500, 200), (352, 500, 200),(528, 1000, 100)]).wait()
-        Sound.play('uq83n-e4wo7.wav').wait()
+        Sound.play('VOLUME_uq83n-e4wo7.wav').wait()
        # Sound.speak('Hello, my name is E V 3!').wait()
         #Sound.speak('I play Tic Tac Toe Motherfucker')
     def run_modes(self, ar, **kwargs):
