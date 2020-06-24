3be31a998a84ce6cde96aea5aab20ae20b67e223
oliver <oliver.lell@gmx.at>
2020-06-24 19:13:15 +0200

    testturn

--- Robot.py
+++ Robot.py
@@ -61,7 +61,9 @@ class Robot:
         #Sound.tone([(264, 500, 200), (352, 500, 200),(528, 1000, 100)]).wait()
         #Sound.play('VOLUME_uq83n-e4wo7.wav').wait()
         Sound.speak('Hello, my name is E V 3!').wait()
-        Sound.speak('I play Tic Tac Toe Motherfucker')
+        Sound.speak('I play Tic Tac Toe Mother')
+        Sound.tone(264, 2000)
+
     def run_modes(self, ar, **kwargs):
         print("rmode: " + str(ar))
      #   print(args.__len__())
